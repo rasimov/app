7d331ec76686b7290c501ed549f6c2a3b5b116bc
Novikov Eugeny <inovykov@softserveinc.com>
2012-08-20 16:32:13 +0300

    tc4
    tc4

---
+++ views/user/add.html.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of add
+ *
+ * @author inovykov
+ */
+class add {
+    //put your code here
+}
+
+?>
